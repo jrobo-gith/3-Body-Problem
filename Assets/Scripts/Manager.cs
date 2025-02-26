2ab8b1293f054aec6ce6a3cf6948ceae5182c5e7
jrobo-gith <jacktheroberts10@gmail.com>
2025-02-26 10:13:13 +0000

    Working not fancy

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -4,74 +4,125 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
-    public float G = 1.0f;
-    public PosVelCalc PVC;
-    public int numBodies = 3;
+
+    public GameObject body;
 
     public List<Vector2> initialPos;
-
+    public List<Vector2> initialVel;
+    public List<Vector2> masses = new List<Vector2>();
     private List<GameObject> bodies;
+
+    public float dt = 0.1f;
+
+    private List<Vector2> velocity =    new List<Vector2>();
+    private List<Vector2> position =    new List<Vector2>();
+    private List<Vector2> forces =       new List<Vector2>();
+
+    public int numBodies = 3;
 
     private void Start()
     {
-        bodies = PVC.spawnBodies(numBodies, initialPos);
-        Debug.Log("AYO");
+        bodies = spawnBodies(numBodies, initialPos);
+        velocity = initialVel;
+        position = initialPos;
+
+        forces = gravity(bodies);
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        foreach (GameObject bod in bodies)
+        // Step 1:
+        for (int i = 0; i < forces.Count; i++)
         {
-            body BS = bod.GetComponent<body>();
-            BS.updateVelocity(bodies);
+            Vector2 force = forces[i];
+            velocity[i] += new Vector2(0.5f, 0.5f) * new Vector2(Time.deltaTime*dt, Time.deltaTime*dt) * force/masses[i];
         }
 
-        foreach (GameObject bod in bodies)
+        // Step 2:
+        for (int i = 0; i < forces.Count; i++)
         {
-            body BS = bod.GetComponent<body>();
-            BS.updatePosition();
+            Vector2 vel = velocity[i];
+            position[i] += vel * Time.deltaTime*dt;
         }
+
+        // Step 3:
+        forces = gravity(bodies);
+        for (int i = 0; i < forces.Count; i++)
+        {
+            Vector2 force = forces[i];
+            velocity[i] += force / masses[i] * (Time.deltaTime*dt / 2);
+        }
+
+        for (int i = 0; i < forces.Count; i++)
+        {
+            GameObject body = bodies[i];
+
+            body.transform.position = position[i];
+        }
+
+
     }
 
-    //public List<Vector2> initialPositions;
-    //public List<Vector2> initialVelocities;
-    //public int numBodies = 3;
-    //public float G = 1.0f;
+    private List<GameObject> spawnBodies(int numBodies, List<Vector2> initPositions)
+    {
+        List<GameObject> bodies = new List<GameObject>();
 
-    //private Dictionary<int, GameObject> bodies = new Dictionary<int, GameObject>();
+        for (int i = 0; i < numBodies; i++)
+        {
+            GameObject B = Instantiate(body, initPositions[i], Quaternion.identity);
+            B.GetComponent<SpriteRenderer>().color = Color.blue;
+            B.transform.localScale = new Vector3(0.3f, 0.3f, 0);
 
-    //void Start()
-    //{
-    //    bodies = PVC.spawnBodies(numBodies, initialPositions);
-    //    updatePositions(bodies, initialVelocities);
+            bodies.Add(B);
+        }
 
-    //}
+        Debug.Log("Instantiation found! Instantiated " + numBodies + " bodies, here is a list of them if you dont believe me:");
+        Debug.Log(bodies);
 
-    //void FixedUpdate()
-    //{
-    //    //List<Vector2> positions = getPositions(bodies);
-    //    //List<Vector2> velocities = PVC.calculateAcc(positions);
-    //    //updatePositions(bodies, velocities);
-    //}
+        return bodies;
+    }
 
-    //private List<Vector2> getPositions(Dictionary<int, GameObject> bodys)
-    //{
-    //    List<Vector2> positions = new List<Vector2>();
+    private List<Vector2> gravity(List<GameObject> bodies)
+    {
+        List<Vector2> f = new List<Vector2>();
 
-    //    foreach (KeyValuePair<int, GameObject> body in bodys)
-    //    {
-    //        Vector2 pos = new Vector2(body.Value.transform.position.x, body.Value.transform.position.y);
-    //        positions.Add(pos);
-    //    }
+        for (int i = 0; i < bodies.Count; i++)
+        {
+            Vector2 ri = bodies[i].transform.position;
+            float ri_mass = masses[i][0];
+            List<float> forces = new List<float>();
+            List<Vector2> directions = new List<Vector2>();
 
-    //    return positions;
-    //}
+            for (int j = 0; j < bodies.Count; j++)
+            {
+                Vector2 rj = bodies[j].transform.position;
 
-    //private void updatePositions(Dictionary<int, GameObject> bodys, List<Vector2> vels)
-    //{
-    //    foreach (KeyValuePair<int, GameObject> body in bodys)
-    //    {
-    //        body.Value.transform.position += new Vector3(vels[body.Key].x, vels[body.Key].y, 0) * Time.deltaTime;
-    //    }
-    //}
+                if (i != j)
+                {
+                    float rj_mass = masses[j][0];
+                    float distance = Vector2.Distance(rj, ri);
+                    forces.Add(calcForce(distance, ri_mass, rj_mass));
+                    directions.Add(calcDirection(ri, rj));
+                }
+            }
+            Vector2 resultantForce = Vector2.zero;
+            for (int k = 0; k < forces.Count; k++)
+            {
+                resultantForce += forces[k] * directions[k];
+            }
+            f.Add(resultantForce);
+        }
+
+        return f;
+    }
+
+    private float calcForce(float distance, float m1, float m2)
+    {
+        return (m1 * m2) / (Mathf.Pow(distance, 2));
+    }
+    private Vector2 calcDirection(Vector2 ri, Vector2 rj)
+    {
+        return (rj - ri) / Vector2.Distance(ri, rj);
+    }
+
 }
--- Assets/Scripts/body.cs
+++ Assets/Scripts/body.cs
@@ -1,50 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class body : MonoBehaviour
-{
-    public Manager manager;
-    public float mass;
-    public Vector3 initialVelocity;
-    Vector3 currentVelocity;
+//public class body : MonoBehaviour
+//{
+//    public Manager manager;
+//    public float mass;
+//    public Vector3 initialVelocity;
+//    Vector3 currentVelocity;
 
-    void Start()
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            initialVelocity = Random.onUnitSphere;
+//    void Start()
+//    {
+//        for (int i = 0; i < 3; i++)
+//        {
+//            initialVelocity = Random.onUnitSphere;
 
-        }
-        currentVelocity = initialVelocity;
-    }
+//        }
+//        currentVelocity = initialVelocity;
+//    }
 
-    public void updateVelocity(List<GameObject> bodies)
-    {
-        foreach (GameObject otherBody in bodies)
-        {
-            if (otherBody != this)
-            {
-                Vector3 diff = otherBody.transform.position - transform.position;
-                float distance = diff.magnitude+1e-6f;
-                Vector3 forceDir = diff / distance;
-                Vector3 force = forceDir * (manager.G * mass / (distance*distance));
-                Vector3 acceleration = force / mass;
+//    public void updateVelocity(List<GameObject> bodies)
+//    {
+//        foreach (GameObject otherBody in bodies)
+//        {
+//            if (otherBody != this)
+//            {
+//                Vector3 diff = otherBody.transform.position - transform.position;
+//                float distance = diff.magnitude+1e-6f;
+//                Vector3 forceDir = diff / distance;
+//                Vector3 force = forceDir * (manager.G * mass / (distance*distance));
+//                Vector3 acceleration = force / mass;
 
-                currentVelocity += acceleration * Time.deltaTime;
+//                currentVelocity += acceleration * Time.deltaTime;
 
-                Debug.Log(diff);
-                Debug.Log(distance);
-                Debug.Log(forceDir);
-                Debug.Log(force);
-                Debug.Log(acceleration);
-                //Debug.Log(currentVelocity);
-            }
-        }
-    }
+//                Debug.Log(diff);
+//                Debug.Log(distance);
+//                Debug.Log(forceDir);
+//                Debug.Log(force);
+//                Debug.Log(acceleration);
+//                //Debug.Log(currentVelocity);
+//            }
+//        }
+//    }
 
-    public void updatePosition()
-    {
-        transform.position += currentVelocity * Time.deltaTime;
-    }
-}
+//    public void updatePosition()
+//    {
+//        transform.position += currentVelocity * Time.deltaTime;
+//    }
+//}
